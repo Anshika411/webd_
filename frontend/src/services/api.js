222559a2078976e9f298749a96a86c44452599da
Anshika Singla <anshika@Anshikas-MacBook-Air.local>
2026-06-03 22:54:37 +0530

    fix api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://careconnect-backend-s3i7....onrender.com/api'
+const API_URL = 'https://careconnect-backend-s3i7.onrender.com/api'
 
 // Create axios instance
 const api = axios.create({
